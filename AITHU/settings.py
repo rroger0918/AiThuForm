d6850b5ce62fc3bfaad1a35a45b94ae0cdbf1513
Jayce0918 <51176006+Jayce0918@users.noreply.github.com>
2021-06-20 14:32:08 +0800

    init commit

--- AITHU/settings.py
+++ AITHU/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.conf import settings
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
-]
-STATIC_URL = '/static/'
+#STATIC_ROOT = (os.path.join(SITE_ROOT, 'static_files/'))
+
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
